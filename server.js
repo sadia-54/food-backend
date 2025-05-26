8ccc9afbd677bb92c10a686fc4616fd3674242d0
jessia <sadiajessia@gmail.com>
2025-05-26 23:33:58 +0600

    vercel

--- server.js
+++ server.js
@@ -1,47 +1,43 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import {connectDB} from './config/db.js';
+import { connectDB } from './config/db.js';
 import foodRouter from './routes/foodRoute.js';
 import userRouter from './routes/userRoute.js';
 import cartRouter from './routes/cartRoute.js';
 import orderRouter from './routes/OrderRoute.js';
 import paymentRouter from './routes/paymentRoute.js';
+import { createServer } from 'http';
+// import { handler } from 'vercel-handler'; // Optional alternative to wrap Express
 
+dotenv.config();
 
-// app config
 const app = express();
-const port = 4000
 
-// dotenv config
-dotenv.config()
-
-//middleware
-app.use(express.json())
-app.use(cors())
+// middleware
+app.use(express.json());
+app.use(cors());
 
 // db connection
-connectDB()
+connectDB();
 
 // api endpoints
-app.use("/api/food", foodRouter)
-app.use("/images", express.static("uploads"))
-app.use("/api/user", userRouter)
-app.use('/api/cart', cartRouter)
-app.use("/api/order", orderRouter)
-app.use("/api/payments", paymentRouter)
+app.use("/api/food", foodRouter);
+app.use("/images", express.static("uploads"));
+app.use("/api/user", userRouter);
+app.use('/api/cart', cartRouter);
+app.use("/api/order", orderRouter);
+app.use("/api/payments", paymentRouter);
 
 app.get('/', (req, res) => {
-    res.send("API is running")
-})
+    res.send("API is running");
+});
 
 app.get('/valobasha', (req, res) => {
-    res.send("API valona")
-})
+    res.send("API valona");
+});
 
-app.listen(port, () => {
-    // console.log(`Server is running on http://0.0.0.0:${port}`)
-})
 
+export default app;
 
 
